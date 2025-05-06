6cbae0a48cb0f1306096bceb547044822c2ae518
Elias Balude <elijahbag456@gmail.com>
2025-05-06 11:36:02 +0300

    minor edit on settings page

--- app/(dashboard)/dashboard/settings/page.tsx
+++ app/(dashboard)/dashboard/settings/page.tsx
@@ -217,8 +217,6 @@ export default function SettingsPage() {
                   <SelectContent>
                     <SelectItem value="male">Male</SelectItem>
                     <SelectItem value="female">Female</SelectItem>
-                    <SelectItem value="non-binary">Non-binary</SelectItem>
-                    <SelectItem value="prefer-not-to-say">Prefer not to say</SelectItem>
                   </SelectContent>
                 </Select>
               </div>
